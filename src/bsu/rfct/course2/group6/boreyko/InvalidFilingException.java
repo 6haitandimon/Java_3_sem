6828cdce53e26f0f88288f092bd52d393a64de44
6haitandimon <borejko2017@gmail.com>
2023-08-19 21:34:31 +0300

    rename exc in "InvalidFilingException"

---
+++ src/bsu/rfct/course2/group6/boreyko/InvalidFilingException.java
@@ -0,0 +1,7 @@
+package bsu.rfct.course2.group6.boreyko;
+
+public class InvalidFilingException extends Exception{
+    public InvalidFilingException(String message){
+        super(message);
+    }
+}
