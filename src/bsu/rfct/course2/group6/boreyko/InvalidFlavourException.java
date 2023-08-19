24c90b984bd4b5a4a23272885babd11bd0a30668
6haitandimon <borejko2017@gmail.com>
2023-08-19 21:30:48 +0300

    add new exc "InvalidFlavourException"

---
+++ src/bsu/rfct/course2/group6/boreyko/InvalidFlavourException.java
@@ -0,0 +1,7 @@
+package bsu.rfct.course2.group6.boreyko;
+
+public class InvalidFlavourException extends Exception{
+    public InvalidFlavourException(String message){
+        super(message);
+    }
+}
