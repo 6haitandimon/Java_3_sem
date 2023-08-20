344e473cda42df45c82c3b6b79ff8a1aa8592c38
6haitandimon <borejko2017@gmail.com>
2023-08-20 21:48:46 +0300

    base app

--- src/bsu/rfct/course2/group6/boreyko/Main.java
+++ src/bsu/rfct/course2/group6/boreyko/Main.java
@@ -1,7 +1,11 @@
 package bsu.rfct.course2.group6.boreyko;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        MainFrame frame = new MainFrame(); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setVisible(true);
     }
 }
