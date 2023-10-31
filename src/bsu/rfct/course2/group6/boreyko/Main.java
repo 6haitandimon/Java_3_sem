b942d36623994f65ba8fccdc7794281cf5b7cfda
6haitandimon <borejko2017@gmail.com>
2023-10-31 11:20:09 +0300

    lab3

--- src/bsu/rfct/course2/group6/boreyko/Main.java
+++ src/bsu/rfct/course2/group6/boreyko/Main.java
@@ -1,7 +1,17 @@
 package bsu.rfct.course2.group6.boreyko;
 
+import javax.swing.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        Double[] a = {1., 5., 4., 3., 2., 1.};
+
+        HornersScheme frame = new HornersScheme(a);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
     }
+
+
 }
